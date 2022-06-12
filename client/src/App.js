023ae51d129ae311b25b43b65d18f8386e27565c
Suman Islam <sumanislam1416@gmail.com>
2022-06-12 20:17:47 +0600

    complete SongList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,16 @@
+import "materialize-css/dist/css/materialize.min.css";
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+import { useEffect } from "react";
+
 import SongList from "./component/SongList.component";
 
 function App() {
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  });
+  
   return (
     <SongList />
   );
--- client/src/component/SongList.component.jsx
+++ client/src/component/SongList.component.jsx
@@ -3,6 +3,7 @@ import { gql, useQuery } from '@apollo/client';
 const songList = gql`
   query getSongs {
     songs {
+      id
       title
     }
   }
@@ -10,13 +11,20 @@ const songList = gql`
 
 function SongList() {
   const { loading, error, data } = useQuery(songList);
-  // console.log(data);
   if (loading) return 'Loading...';
   if (error) return `Error! ${error.message}`;
-  console.log(data);
+
   return (
-    <div>
-      SongList
+    <div className='collection'>
+      {
+        data.songs.map((song) => {
+          return (
+            <li key={song.id} className='collection-item' >
+              {song.title}
+            </li>
+          )
+        })
+      }
     </div>
   )
 };
