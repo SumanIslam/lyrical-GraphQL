eab7a03eb24aa2832763631c717431a5b94c3f12
Suman Islam <sumanislam1416@gmail.com>
2022-06-17 22:04:07 +0600

    complete createSong component and implement addSong Mutation successfully and then navigate to homepage

--- client/src/component/songCreate.component.jsx
+++ client/src/component/songCreate.component.jsx
@@ -1,15 +1,37 @@
 import { useState } from "react";
+import { useMutation, gql } from "@apollo/client";
+import { useNavigate } from "react-router";
+
+const ADD_SONG = gql`
+  mutation AddSong($title:String){
+    addSong(title: $title) {
+      title
+    }
+  }
+`
 
 function SongCreate() {
   const [title, setTitle] = useState('');
+  const [addSong, { data, loading, error }] = useMutation(ADD_SONG);
+
+  let navigate = useNavigate();
+
+  if (loading) return 'Submitting...';
+  if (error) return `Submission error! ${error.message}`;
 
   const changeTitle = (e) => {
     setTitle(e.target.value);
   }
+
   return(
     <div className="container">
       <h3>Create a new Song</h3>
-      <form>
+      <form onSubmit={e => {
+        e.preventDefault();
+        addSong({ variables: {title: title}});
+        setTitle('');
+        navigate('/')
+      }}>
         <label htmlFor="">Song Title: </label>
         <input
           onChange={changeTitle}
