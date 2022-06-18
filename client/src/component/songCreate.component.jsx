875155e8039d6db192df75bc13116f2c2d3afc82
Suman Islam <sumanislam1416@gmail.com>
2022-06-18 13:44:56 +0600

    add Back link to navite from /song/create to /

--- client/src/component/songCreate.component.jsx
+++ client/src/component/songCreate.component.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useMutation, gql } from "@apollo/client";
 import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
 
 const ADD_SONG = gql`
   mutation AddSong($title:String){
@@ -25,6 +26,9 @@ function SongCreate() {
 
   return(
     <div className="container">
+      <Link to="/">
+        <h5>Back</h5>
+      </Link>
       <h3>Create a new Song</h3>
       <form onSubmit={e => {
         e.preventDefault();
